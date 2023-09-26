4c06ad316cdcc4807ad4d7410ace20036c6eac0d
DailyDev59 <daily1963@yandex.ru>
2023-09-26 10:35:28 +0300

    Добавлены slick слайдеры 26.09.2023

--- js/main.js
+++ js/main.js
@@ -19,5 +19,37 @@ function init() {
  // Перемещаем параграф из #myDiv1 в #myDiv2
 
   $('#nets').append( $('#myDiv1>p') );
-
 }
+
+$(document).ready(function(){
+    $('.slider-1__items').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: false,
+        initialSlide: 0        
+    });
+});
+
+$(document).ready(function(){
+    $('.slider-2__items').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: false,
+        initialSlide: 0        
+    });
+});
+
+$(document).ready(function(){
+    $('.coaches__items').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: false,
+        initialSlide: 0        
+    });
+});
